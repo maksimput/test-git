67bfa1cfaedb6b89daa8a52012e46eba090ba406
maksimput <82806350+maksimput@users.noreply.github.com>
2023-11-26 21:30:55 +0500

    New file test_script

---
+++ test_script.js
@@ -0,0 +1,7 @@
+function domainName(url){
+    url = url.replace("https://", '');
+    url = url.replace("http://", '');
+    url = url.replace("www.", '');
+    return url.split('.')[0];
+  };
+  console.log(domainName('www.google.com'))
