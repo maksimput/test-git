55889caf143f4a192ccfef901eaee47fdcc645c7
maksimput <82806350+maksimput@users.noreply.github.com>
2023-11-26 22:50:43 +0500

    Add comment

--- test_script.js
+++ test_script.js
@@ -1,7 +1,7 @@
 function domainName(url){
-    url = url.replace("https://", '');
+    url = url.replace("https://", ''); //del start url
     url = url.replace("http://", '');
     url = url.replace("www.", '');
-    return url.split('.')[0];
+    return url.split('.')[0]; //return domain
   };
   console.log(domainName('www.yandex.ru'))
